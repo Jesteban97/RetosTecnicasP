1b3793353c65a28f1a1cf708f2c0dbf08ca1c989
Jesteban97 <90581557+Jesteban97@users.noreply.github.com>
2022-01-30 21:16:00 -0500

    Add files via upload

--- Reto5/src/main/java/tecnicasprogramacion/reto5/Pokebola.java
+++ Reto5/src/main/java/tecnicasprogramacion/reto5/Pokebola.java
@@ -4,23 +4,36 @@ package tecnicasprogramacion.reto5;
 public class Pokebola {
     //atributos
     private String tamaño;
-    private Pokemon Pokemon;
+    private Pokemon pokemon;
     private boolean vacia = false;
     //constructor
 
-    public Pokebola(String tamaño, Pokemon Pokemon, boolean vacia) {
+    public Pokebola(String tamaño, Pokemon pokemon, boolean vacia) {
         this.tamaño = tamaño;
-        this.Pokemon = Pokemon;
+        this.pokemon = pokemon;
         this.vacia = vacia;
     }
     
     //metodos
     public void Atrapar(Pokemon x){
-        
+        if (this.vacia == true ){
+            this.pokemon = x;
+            this.vacia = false;
+        }
+        else {
+            System.out.println("Esta pokebola esta ocupada");
+        }
     }
     
     public void Liberar(Pokemon x){
-        
+        if (this.vacia == false ){
+            this.vacia = true;
+            this.pokemon = null;
+            System.out.println("El pokemon "+x+" ha sido liberado");
+        }
+        else {
+            System.out.println("Esta pokebola esta vacia");
+        }
     }
     //getter and setter
 
@@ -33,11 +46,11 @@ public class Pokebola {
     }
 
     public Pokemon getPokemon() {
-        return Pokemon;
+        return pokemon;
     }
 
     public void setPokemon(Pokemon Pokemon) {
-        this.Pokemon = Pokemon;
+        this.pokemon = Pokemon;
     }
 
     public boolean isVacia() {
