885e090b7ef7ad9e3fa7a522a9ab5822edb4b1e8
Jose Waldo <josferw@gmail.com>
2022-02-13 15:42:10 -0500

    Agregué la clase Carretera, falta hacer el último método y revisar los demas, también falta hacer los test en Main.

--- Reto7/src/main/java/com/mycompany/reto7/Carretera.java
+++ Reto7/src/main/java/com/mycompany/reto7/Carretera.java
@@ -6,12 +6,64 @@ import java.util.List;
 
 public class Carretera {
     
-    private List tramos; 
+    private List<TramoGenerico> tramos; 
 
     public Carretera() {
-        this.tramos = new ArrayList();
+        this.tramos = new ArrayList<TramoGenerico>();
     }
 
+    public List getTramos() {
+        return tramos;
+    }
     
+    public void añadirTramo(TramoGenerico tramo) {
+        if(tramo != null){
+            this.tramos.add(tramo);
+        }
+    }
+    
+    public double longitudTotal() {
+        double total = 0;
+        for (TramoGenerico tramo : tramos) {
+            total += tramo.longitud();
+        }
+        return total;
+    }
+    
+    public double areaTotal() {
+        double total = 0;
+        for (TramoGenerico tramo : tramos) {
+            total += tramo.area();
+        }
+        return total;
+    }
+    
+    public double volumenMaterialTotal() {
+        double total = 0;
+        for (TramoGenerico tramo : tramos) {
+            total += tramo.volumen();
+        }
+        return total;
+    }
+    
+    public double volumenAsfaltoTotal() {
+        double total = 0;
+        for (TramoGenerico tramo : tramos) {
+            if(tramo.getClass().getName().equals("Carretera: com.mycompany.reto7.TramoAsfalto")){
+                total += tramo.volumen();
+            }           
+        }
+        return total;
+    }
+    
+    public double volumenSinAsfaltoTotal() {
+        double total = 0;
+        for (TramoGenerico tramo : tramos) {
+            if(tramo.getClass().getName().equals("Carretera: com.mycompany.reto7.TramoSinAsfaltar")){
+                total += tramo.volumen();
+            }           
+        }
+        return total;
+    }
     
 }
--- Reto7/src/main/java/com/mycompany/reto7/Main.java
+++ Reto7/src/main/java/com/mycompany/reto7/Main.java
@@ -1,10 +1,28 @@
+/*
+
+Constructor clase asfaltada:
+TramoAsfalto(double xInicial, double yInicial, double xFinal, double yFinal, String proveedorAsfalto, double velocidadMax, boolean prohibidoAdelantar)
+
+Constructor clase sin asfaltar:
+public TramoSinAsfaltar(double xInicial, double yInicial, double xFinal, double yFinal, String tipoMaterial, double espesor)
+
+*/
 
 package com.mycompany.reto7;
 
 public class Main {
 
     public static void main(String[] args) {
+        Carretera carretera = new Carretera();
         
+        TramoGenerico tramo1 = new TramoAsfalto(1, 1, 2, 2, "Pepito", 80, true);
+        TramoGenerico tramo2 = new TramoSinAsfaltar(2, 3, 4, 5, "piedra", 0.35);
+        
+        carretera.añadirTramo(tramo1);
+        carretera.añadirTramo(tramo2);
+        
+        System.out.println("Carretera: " + tramo1.getClass().getSimpleName());;
+        System.out.println("Carretera: " + tramo2.getClass().getSimpleName());;
        
     }
     
